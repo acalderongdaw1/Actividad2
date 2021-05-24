d17caaa10385709273f5895c850c9313f4e4e749
acalderongdaw1 <acalderongdaw1@gmail.com>
2021-05-24 19:01:27 +0200

    Texto repetido cambiado por constante

--- PiedraPapelTijera/src/ende/Juego.java
+++ PiedraPapelTijera/src/ende/Juego.java
@@ -9,6 +9,9 @@ public class Juego
 {
     public static void main(String args[])
     {
+    	final String j1Gana = "Jugador 1 GANA";
+    	final String j2Gana = "Jugador 2 GANA";
+    	
         Jugador p1 = new Jugador();
         Jugador p2 = new Jugador();
         boolean finDeJuego = false;  
@@ -33,23 +36,23 @@ public class Juego
             }
             else if((opcionJugador1.equals("papel")) && (opcionJugador2.equals("piedra"))) {
             	exitosJugador1 = ++p1.exitos;
-                System.out.println("Jugador 1 GANA");
+                System.out.println(j1Gana);
             }
             else if((opcionJugador1.equals("piedra")) && (opcionJugador2.equals("tijeras"))) {
             	exitosJugador1 = ++p1.exitos;
-                System.out.println("Jugador 1 GANA");
+                System.out.println(j1Gana);
             }
             else if((opcionJugador1.equals("tijeras")) && (opcionJugador2.equals("piedra"))) {
             	exitosJugador2 = ++p2.exitos;
-                System.out.println("Jugador 2 GANA");
+                System.out.println(j2Gana);
             }
             else if((opcionJugador1.equals("tijeras")) && (opcionJugador2.equals("papel"))) {
             	exitosJugador1 = ++p1.exitos;
-                System.out.println("Jugador 1 GANA");
+                System.out.println(j1Gana);
             }
             else if((opcionJugador1.equals("papel")) && (opcionJugador2.equals("tijeras"))) {
             	exitosJugador2 = ++p2.exitos;
-                System.out.println("Jugador 2 GANA");
+                System.out.println(j2Gana);
             }
             if(opcionJugador1 == opcionJugador2) {
             	empates++;
@@ -61,7 +64,7 @@ public class Juego
                 System.out.println("FIN DEL JUEGO!!");
             }
             System.out.println();
-        } while(finDeJuego!=true);
+        } while(finDeJuego != true);
     }
 }
 /**
